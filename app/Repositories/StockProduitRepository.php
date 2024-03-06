8ebf363d8a710fd949741ae060b55cd1640a6097
djamala xavier <xavier.djamala@softexpertise.com>
2024-03-06 12:49:50 +0000

    requete pour afficher les produits disponible dans le stock

--- app/Repositories/StockProduitRepository.php
+++ app/Repositories/StockProduitRepository.php
@@ -20,11 +20,10 @@ class StockProduitRepository extends Repository
 
         $bakehouse_id = (Auth::user()->bakehouse) ? Auth::user()->bakehouse->id : NULL ;
 
-        $query = StockProduct::selectRaw('products_stock.price, products_stock.product_id, products_stock.id, products.name AS product_name, products.image, products_stock.quantity, SUM(products_stock.quantity* products_stock.price) AS stock_value')
+        $query = StockProduct::selectRaw('products.price, products_stock.product_id, products_stock.id, products.name AS product_name, products.image, products_stock.quantity')
                     ->where('products.type', 1)
                     ->where('products_stock.bakehouse_id', $bakehouse_id)
                     ->leftJoin('products', 'products.id', '=', 'products_stock.product_id')
-                    ->groupByRaw('products_stock.product_id, products_stock.price')
                     ->get();
 
         return $query;
