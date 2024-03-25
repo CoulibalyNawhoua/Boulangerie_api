5508a0ae449b55c033e5802e5417c00532202d2b
Ambeu <69042263+Ambeu@users.noreply.github.com>
2024-03-25 10:25:48 +0000

    role commit

--- app/Repositories/RoleRepository.php
+++ app/Repositories/RoleRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Http\Requests\StoreRoleRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -43,18 +44,18 @@ class RoleRepository extends Repository
     public function role_view($id)
     {
 
-        $role = $this->model->find($id);
+        $role = Role::findById($id);
 
-        $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$id)
-            ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
-            ->all();
+        if (!$role) {
+            return response()->json(['error' => 'Role introuvable'], 404);
+        }
+
+        $rolePermissions = $role->permissions->pluck('name');
 
         $data['role'] = $role;
-
         $data['rolePermission'] = $rolePermissions;
 
         return $data;
-        return $this->model->find($id);
     }
 
 
